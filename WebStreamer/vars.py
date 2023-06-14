e0e74cf022a78a5a3de031ff415ee9ca1bb73289
Mahakaal101 <124979598+Mahakaal101@users.noreply.github.com>
2023-06-15 00:07:02 +0530

    Update vars.py

--- WebStreamer/vars.py
+++ WebStreamer/vars.py
@@ -14,7 +14,7 @@ class Var(object):
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
     BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1001813214601'))
-    PORT = int(getenv('PORT', 8080))
+    PORT = int(getenv('PORT', 443))
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '34.230.4.214'))
     OWNER_ID = int(getenv('OWNER_ID', '5152847809'))
     NO_PORT = bool(getenv('NO_PORT', False))
